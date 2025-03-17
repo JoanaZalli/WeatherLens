e521c617d44dc4058d35a7c05a45fc84f549d783
jzalli <joana.zalli@cardoai.com>
2025-03-17 17:48:33 +0100

    testing pipeline

---
+++ src/Infrastructure/Data/Migrations/20250317164815_another_test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherLens.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class another_test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
